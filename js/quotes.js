1000a288f62e56a1b9e798b82a8bcdbce81df698
NALLI NITHIN <37226644+nithinnalli@users.noreply.github.com>
2018-03-10 11:57:45 +0530

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.category_item').click(function(){
+		var category = $(this).attr('id');
+		if(category=='all')
+		{
+			$('.picture_item').addClass('hide');
+			setTimeout(function(){
+				$('.picture_item').removeClass('hide');
+			},300);
+		}
+		else
+		{
+			$('.picture_item').addClass('hide');
+			setTimeout(function(){
+				$('.'+category).removeClass('hide');
+			},300);
+		}
+	})
+});
